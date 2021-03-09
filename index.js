1592b2ce551b2c019aa9e94e24d947fdc12f3551
nils-vanderwerf <n.vanderw.92@gmail.com>
2021-03-09 19:02:05 +1100

    Passed all tests

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+
+
+function returnFirstTwoDrivers(allDrivers) {
+    let myNewArray = allDrivers.slice(0, 2);
+    return myNewArray;
+}
+
+function returnLastTwoDrivers(allDrivers) {
+    let myNewArray = allDrivers.slice(-2);
+    return myNewArray;
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(num1) {
+    return function(num2) {
+        return num1 * num2
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(arrayOfDrivers, returnLastTwoDrivers){
+
+    let driversToReturn = returnLastTwoDrivers(arrayOfDrivers);
+
+    return driversToReturn;
+
+}
